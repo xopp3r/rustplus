307a6c51682c32bb62af36a893311130e6e4e8ad
xopp3r <shikalovslava@gmail.com>
2026-07-06 01:14:09 +0300

    Fix duplicate log messages after recreation of RustSocket

--- rustplus/rust_api.py
+++ rustplus/rust_api.py
@@ -60,14 +60,15 @@ class RustSocket:
         self.server_details = server_details
         self.command_options = command_options
         self.logger = logging.getLogger("rustplus.py")
-
-        console_handler = logging.StreamHandler()
-        console_handler.setLevel(logging.DEBUG)
-        console_handler.setFormatter(
-            logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
-        )
-        self.logger.addHandler(console_handler)
-        self.logger.setLevel(logging.DEBUG)
+        
+        if not self.logger.handlers:
+            console_handler = logging.StreamHandler()
+            console_handler.setLevel(logging.DEBUG)
+            console_handler.setFormatter(
+                logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+            )
+            self.logger.addHandler(console_handler)
+            self.logger.setLevel(logging.DEBUG)
 
         self.ws = RustWebsocket(
             self.server_details,
